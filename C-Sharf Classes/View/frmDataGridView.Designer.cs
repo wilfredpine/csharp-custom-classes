c771aff3a8bb14faaec022328bcd8ae24b3be2b1
Wilfred V. Pine <53922574+redmalmon@users.noreply.github.com>
2021-02-16 11:55:00 +0800

    Add files via upload

---
+++ C-Sharf Classes/View/frmDataGridView.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace C_Sharf_Classes.View
+{
+    partial class frmDataGridView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.dgvUsers = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.btnSearch);
+            this.panel3.Controls.Add(this.txtUsername);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.dgvUsers);
+            this.panel3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel3.Location = new System.Drawing.Point(9, 9);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(672, 418);
+            this.panel3.TabIndex = 6;
+            // 
+            // dgvUsers
+            // 
+            this.dgvUsers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsers.Location = new System.Drawing.Point(3, 49);
+            this.dgvUsers.Name = "dgvUsers";
+            this.dgvUsers.Size = new System.Drawing.Size(666, 366);
+            this.dgvUsers.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Search Username:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(151, 17);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(184, 26);
+            this.txtUsername.TabIndex = 2;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(341, 16);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(108, 27);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // frmDataGridView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(690, 436);
+            this.Controls.Add(this.panel3);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmDataGridView";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DataGridView";
+            this.Load += new System.EventHandler(this.frmDataGridView_Load);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsers)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.DataGridView dgvUsers;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ C-Sharf Classes/View/frmDataGridView.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using C_Sharf_Classes.Classes;
+
+namespace C_Sharf_Classes.View
+{
+    public partial class frmDataGridView : Form
+    {
+        Database db = new Database();
+
+        public frmDataGridView()
+        {
+            InitializeComponent();
+        }
+
+        void loadUsers()
+        {
+            //optional
+            string[] customheader = { "User ID", "Username", "Gender" };
+
+            //table methods
+            db.table("select userid,username,sex from users", dgvUsers, customheader);
+        }
+
+        void searchUser()
+        {
+            //optional
+            string[] customheader = { "User ID", "Username", "Gender" };
+
+            //table methods
+            db.table("select userid,username,sex from users where username='"+ txtUsername.Text +"'", dgvUsers, customheader);
+        }
+
+        private void frmDataGridView_Load(object sender, EventArgs e)
+        {
+            loadUsers();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            searchUser();
+        }
+    }
+}
