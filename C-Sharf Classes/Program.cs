389341228036fc86222954f2255967d11e37921d
Wilfred V. Pine <53922574+redmalmon@users.noreply.github.com>
2021-02-16 11:51:23 +0800

    Add files via upload

---
+++ C-Sharf Classes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using C_Sharf_Classes.Classes;
+using C_Sharf_Classes.View;
+
+namespace C_Sharf_Classes
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Database db = new Database();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmLogin());
+        }
+    }
+}
